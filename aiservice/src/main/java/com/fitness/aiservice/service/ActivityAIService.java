c2c6c60859e5e33e00af3a52e41b3f292f1ab6f7
SudhaA <sudhasavi2@gmail.com>
2026-01-11 12:29:36 +0530

    gemini service integration

---
+++ aiservice/src/main/java/com/fitness/aiservice/service/ActivityAIService.java
@@ -0,0 +1,13 @@
+package com.fitness.aiservice.service;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class ActivityAIService {
+    private final GeminiService geminiService;
+     
+
+}
---
+++ aiservice/src/main/java/com/fitness/aiservice/service/GeminiService.java
@@ -0,0 +1,36 @@
+package com.fitness.aiservice.service;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+import java.util.HashMap;
+import java.util.Map;
+import org.springframework.beans.factory.annotation.Value;
+
+
+@Service
+public class GeminiService {
+    private final WebClient webClient;
+     
+    @Value("${gemini.api.url}")
+    private String apiUrl = "geminiApiUrl";
+    @Value("${gemini.api.key}")
+    private String apiKey = "geminiApiKey";    
+    
+    public GeminiService(WebClient.Builder webClientBuilder) {
+        this.webClient = webClientBuilder.baseUrl(apiUrl).build();
+    }
+
+    public String generateContent(String prompt) {
+        Map<String,Object> requestBody = Map.of("contents",
+         new Object[] { Map.of("parts", new Object[] { Map.of("text", prompt) } ) });
+        // Implementation for calling Gemini API
+        // This is a placeholder implementation
+        return webClient.post()
+                .uri(apiUrl + apiKey)
+                .header("Content-Type", "application/json")
+                .bodyValue(requestBody)
+                .retrieve()
+                .bodyToMono(String.class)
+                .block();
+    }
+
+}
